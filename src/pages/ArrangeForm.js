9d3473e6775a7ae45a3b0d7406e729dc3dce2925
ulaista <istamovulugbek@yandex.ru>
2024-03-09 13:04:43 +0300

    ui logic after sending order

--- src/pages/ArrangeForm.js
+++ src/pages/ArrangeForm.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { useCart } from "../CartContext";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from 'react-router-dom';
 import instanceApi from '../axiosConfig';
 import PageHeader from "../components/arrangeFormComponents/PageHeader";
 import { getLocalizedField } from '../utils/localizedfield';
 import { serverURL } from "../axiosConfig";
 
 function CheckoutPage() {
-  const { cart } = useCart();
+  const { cart, dispatch } = useCart();
   const { t } = useTranslation();
+  const navigate = useNavigate(); 
+
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const [customerInfo, setCustomerInfo] = useState({
     fullname: '',
     phone_number: '',
     message: '',
     address: '',
   });
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleChange = (e) => {
     setCustomerInfo({ ...customerInfo, [e.target.name]: e.target.value });
@@ -46,14 +56,41 @@ function CheckoutPage() {
     instanceApi.post('/checkout/', submissionData)
       .then(response => {
         console.log("Order submitted successfully", response.data);
-        // Actions after successful submission, e.g., clearing the cart or redirecting the user
+        setIsSubmitted(true); // Update submission status
+        dispatch({ type: "CLEAR_CART" }); // Очистите корзину через dispatch
+        setTimeout(() => {
+          navigate('/'); // Redirect after 3 seconds
+        }, 3000);
       })
       .catch(error => {
         console.error("There was an error submitting the order", error);
-        // Error handling, e.g., displaying a message to the user
+        // Error handling
       });
   };
-
+  if (isSubmitted) {
+    return (
+      <>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      backgroundColor: '#f0f4f8',
+      color: '#4BB543',
+      fontSize: '24px',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      animation: 'fadeIn 2s'
+    }}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16" style={{ marginBottom: '20px', color: '#4BB543' }}>
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.525 7.465a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.99-4.99a.75.75 0 0 0-.01-1.05z"/>
+      </svg>
+      Successfully sent
+    </div>
+    </>
+    );
+  } else {
   return (
     <>
     <PageHeader/>
@@ -168,6 +205,7 @@ function CheckoutPage() {
       </section>
     </>
   );
+  }
 }
 
 export default CheckoutPage;
